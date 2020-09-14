41ff6b1b8a57a58bcc05a9f348672bf461552104
Grigore Stefan <g_stefan@yahoo.com>
2020-09-14 03:08:55 +0300

    update

--- port/build.msvc.vendor.cmd
+++ port/build.msvc.vendor.cmd
@@ -13,6 +13,7 @@ set WEB_LINK=https://github.com/win-iconv/win-iconv/archive/v0.0.8.zip
 if not exist %VENDOR%.zip curl --insecure --location %WEB_LINK% --output %VENDOR%.zip
 7z x %VENDOR%.zip -aoa -o.
 del /F /Q %VENDOR%.zip
+if exist %VENDOR%.7z del /F /Q %VENDOR%.7z
 7zr a -mx9 -mmt4 -r- -sse -w. -y -t7z %VENDOR%.7z %VENDOR%
 rmdir /Q /S %VENDOR%
 popd
