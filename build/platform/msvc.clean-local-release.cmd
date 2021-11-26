112fc08fd21c9905b93aebdea425aba2bfe2f6fe
Grigore Stefan <g_stefan@yahoo.com>
2021-11-26 03:19:43 +0200

    Update

--- build/platform/msvc.clean-local-release.cmd
+++ build/platform/msvc.clean-local-release.cmd
@@ -3,6 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> version-dependency-update vendor-win-iconv
+echo - %BUILD_PROJECT% ^> clean-local-release
 
-xyo-cc --update-version-dependency vendor-win-iconv
+if exist release\ rmdir /Q /S release
